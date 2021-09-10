46c8fc1568938dff11de743da55028d215c1b192
Daniel Felipe Rincon Muñoz <daniel.rincon@masiv.com>
2021-09-09 21:05:50 -0500

    Added auth controller

---
+++ src/main/java/org/ada/school/utils/Constants.java
@@ -0,0 +1,8 @@
+package org.ada.school.utils;
+
+public class Constants {
+
+    public static final String CLAIMS_ROLES_KEY = "abc123";
+    public static final int TOKEN_DURATION_MINUTES = 10;
+
+}
